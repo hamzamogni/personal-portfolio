63435df66c194695f2a4ee28d162710162a95d6e
Hamza Mogni <hamzamogni5@gmail.com>
2022-05-29 20:59:58 +0100

    set social media links

--- components/Header/SocialMedia.vue
+++ components/Header/SocialMedia.vue
@@ -2,7 +2,7 @@
   <v-row justify="center">
     <v-col v-for="(social, idx) in social_media" :key="idx" class="flex-grow-0">
       <v-btn icon :href="social.href" target="_blank">
-        <v-icon>{{ social.icon }}</v-icon>
+        <v-icon large>{{ social.icon }}</v-icon>
       </v-btn>
     </v-col>
   </v-row>
@@ -13,9 +13,9 @@ export default {
   data() {
     return {
       social_media: [
-        { icon: "mdi-github", href: "https://github.com/safoinme" },
-        { icon: "mdi-linkedin", href: "https://www.linkedin.com/in/safoinme" },
-        { icon: "mdi-twitter", href: "https://twitter.com/safoinme" },
+        { icon: "mdi-github", href: "https://github.com/hamzamogni" },
+        { icon: "mdi-linkedin", href: "https://www.linkedin.com/in/hamza-mogni" },
+        { icon: "mdi-twitter", href: "https://twitter.com/hamza_mogni" },
       ],
     };
   },
