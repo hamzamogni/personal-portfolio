5c7d3e45be2a7e4b1f9fac5999f8dfd9b3238804
Hamza Mogni <hamzamogni5@gmail.com>
2022-05-29 20:57:37 +0100

    change font to 'Sources Sans Pro'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,89 +1,88 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        titleTemplate: '%s - personal-portfolio',
-        title: 'personal-portfolio',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [{
-                charset: 'utf-8'
-            },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: ''
-            },
-            {
-                name: 'format-detection',
-                content: 'telephone=no'
-            }
-        ],
-        link: [{
-            rel: 'icon',
-            type: 'image/x-icon',
-            href: '/favicon.ico'
-        }]
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: "%s - personal-portfolio",
+    title: "personal-portfolio",
+    htmlAttrs: {
+      lang: "en",
     },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [],
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-        // https://go.nuxtjs.dev/vuetify
-        '@nuxtjs/vuetify',
-        '@nuxtjs/google-fonts'
-    ],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        '@nuxt/content'
-    ],
-
-  googleFonts: {
-      families: {
-        "Fira Code": true
+    meta: [
+      {
+        charset: "utf-8",
       },
-    display: "swap"
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "",
+      },
+      {
+        name: "format-detection",
+        content: "telephone=no",
+      },
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico",
+      },
+    ],
   },
 
-    content: {
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
 
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    "@nuxtjs/vuetify",
+    "@nuxtjs/google-fonts",
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ["@nuxt/content"],
+
+  googleFonts: {
+    families: {
+      "Source Sans Pro": true,
     },
+    display: "swap",
+  },
 
-    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-    vuetify: {
-        customVariables: ['~/assets/variables.scss'],
-      treeShake: true,
-        theme: {
-            dark: false,
-            themes: {
-                dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
-                }
-            }
-        }
+  content: {},
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    treeShake: true,
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
     },
+  },
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {}
-}
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
