7916de5d1bb373da6a1a62487bccf303c6d409f1
Hamza Mogni <hamzamogni5@gmail.com>
2022-05-30 08:54:41 +0100

    add image to article page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,10 +1,11 @@
 <template>
   <span>
     <p class="mb-0 mt-10" id="title">{{ article.title }}</p>
-    <span class="text-subtitle-2 font-weight-medium">
+    <span class="text-subtitle-2 font-weight-medium text--disabled ml-3">
       {{ formatDate(article.createdAt) }} - 20 min read
     </span>
-    <nuxt-content class="mt-5 text-h6" :document="article" tag="article"></nuxt-content>
+    <v-img :src="article.img" class="elevation-1 mt-4"></v-img>
+    <nuxt-content class="mt-10 text-h6" :document="article" tag="article"></nuxt-content>
   </span>
 </template>
 
@@ -13,7 +14,7 @@ export default {
   layout: "blog-index",
   async asyncData({ $content, params }) {
     const article = await $content("articles", params.slug).fetch();
-
+    console.log(article.toc);
     return { article };
   },
   methods: {
@@ -40,7 +41,7 @@ h6 {
 }
 
 #title {
-  font-size: 2.7rem;
+  font-size: 2rem;
   font-weight: 900;
 }
 
