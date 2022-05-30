88779a87e1d8ddf08e9a148daf47a374cb0aff16
Hamza Mogni <hamzamogni5@gmail.com>
2022-05-30 08:53:53 +0100

    add social media to contac page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,26 @@
 <template>
+  <div>
   <nuxt-content class="mt-5 text-h6" :document="article"></nuxt-content>
+
+  <v-row class="flex-column mt-10" justify="center">
+    <v-col v-for="(social, idx) in social_media" :key="idx" cols="4" align-self="center">
+      <v-card to="social.url" outlined rounded="lg">
+        <v-card-text>
+          <v-btn icon :href="social.url" target="_blank">
+            <v-icon x-large>{{ social.icon }}</v-icon>
+          </v-btn>
+            <span class="text-h6 ml-5">
+              {{ social.name }}
+            </span>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+  </div>
 </template>
 
 <script>
+import { getSocialLinks } from '~/services/social-links';
 export default {
   layout: "blog-index",
   async asyncData({ $content, params }) {
@@ -10,6 +28,12 @@ export default {
     console.log(article);
 
     return { article };
+  },
+
+  data() {
+    return {
+      social_media: getSocialLinks(),
+    };
   },
 };
 </script>
---
+++ services/social-links.js
@@ -0,0 +1,8 @@
+
+export function getSocialLinks() {
+  return [
+      {"name": "github",  "icon": "mdi-github", "url": "https://github.com/hamzamogni"},
+      {"name": "twitter", "icon": "mdi-twitter",  "url": "https://twitter.com/hamza_mogni"},
+      {"name": "linkedin", "icon": "mdi-linkedin", "url": "https://www.linkedin.com/in/hamza-mogni"},
+  ]
+}
