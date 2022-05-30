6c95929796e39cc95921414c888fb231032b1ab0
Hamza Mogni <hamzamogni5@gmail.com>
2022-05-30 06:07:07 +0100

    capitalize name in appbar

--- components/Appbar.vue
+++ components/Appbar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar app elevation="0" color="transparent" class="mx-16 px-16" absolute>
     <v-toolbar-title>
-      <v-btn href="/" text class="text-h6 font-weight-black">
+      <v-btn href="/" text class="text-h6 text-capitalize font-weight-black">
         Hamza Mogni
       </v-btn>
     </v-toolbar-title>
