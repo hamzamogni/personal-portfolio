d1eb035623090bff0a3c0c50b1fc306719942777
Hamza Mogni <hamzamogni5@gmail.com>
2023-03-01 14:50:26 +0100

    disable analytics on dev environment

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 export default (context, inject) => {
+    let id = process.env.NODE_ENV === 'development' ? '' : 'G-6YG2573Y2D'
+
     Vue.use(
         VueGtag,
         {
-            config: { id: 'G-6YG2573Y2D' },
+            config: { id: id },
             pageTrackerScreenviewEnabled: true,
         },
         context.app.router
